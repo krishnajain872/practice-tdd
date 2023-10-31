a51d65b49ba393d505b510be42c52f17971fa15b
krishna jain <krishnajain@gkmit.co>
2023-10-31 13:26:25 +0530

    feature account model and migration

---
+++ migrations/20231031074815-create-account.js
@@ -0,0 +1,42 @@
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("accounts", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+      },
+      account_type: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      balance: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("accounts");
+  },
+};
---
+++ models/Account.js
@@ -0,0 +1,48 @@
+"use strict";
+const { Model, UUID } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Account extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Account.hasOne(models.User, {
+        as: "account_details",
+      });
+
+      Account.belongsToMany(models.Transaction, { as: "acount_details" });
+    }
+  }
+  Account.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUID,
+      },
+      account_type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      balance: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0.0,
+      },
+      user_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Account",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    }
+  );
+  return Account;
+};
