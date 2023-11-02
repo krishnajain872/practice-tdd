3baa9e026d2c3dd50d3678903a18682d41e64583
krishna jain <krishnajain@gkmit.co>
2023-11-02 12:38:37 +0530

    feature error and response helper

---
+++ helpers/responseHelp.js
@@ -0,0 +1,7 @@
+module.exports.responseHelper = error = (code, success, message, payload) => {
+  return {
+    code: code,
+    success: success,
+    data: { message: message, payload: payload },
+  };
+};
