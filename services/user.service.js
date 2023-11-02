eca5408008f69201a3ac8682712101ee6896cc51
krishna jain <krishnajain@gkmit.co>
2023-11-02 13:03:28 +0530

    feature user register service

--- services/user.service.js
+++
@@ -1,5 +0,0 @@
-const { USERS } = require("../helpers/fakeUser");
-const User = require("../models/User");
-
-data = JSON.stringify(USERS());//fake data
-module.exports.register = async () => {};
--- services/user.services.js
+++ services/user.services.js
@@ -1,19 +1,42 @@
 const db = require("../models");
 const User = db.User;
 
+const errorHelper = require("./../helpers/errorHelp");
 const userRegistrationService = async (payload) => {
   try {
-    const [userdata] = await User.create(payload);
+    if (
+      !payload.email &&
+      !payload.mobile &&
+      !payload.password &&
+      !payload.first_name &&
+      !payload.last_name
+    ) {
+      return errorHelper(
+        "Request Not accept unprocceble payload",
+        422,
+        "service error",
+        "please check the payload and try again"
+      );
+    }
+    const pass = passHashHelper(payload.password);
+    const data = {
+      email: payload.email,
+      mobile: payload.mobile,
+      password,
+      first_name: payload.first_name,
+      last_name: payload.last_name,
+    };
+    const [userdata] = await User.create(data);
     return userdata;
   } catch (err) {
-    const errors = {
-      error: "Service Error",
-      code: 409,
-      name: err.name,
-      message: "user already registered",
-      actual: err.parent.detail,
-    };
-    return errors
+    const errors = errorHelper(
+      err,
+      409,
+      err.name,
+      "please check the payload and try again",
+      err.parent.detail
+    );
+    return errors;
   }
 };
 
