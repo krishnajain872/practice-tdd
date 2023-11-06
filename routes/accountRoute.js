627bfdbbe7b601dbe3ddf8a133ecf0380b5a0573
krishna jain <krishnajain@gkmit.co>
2023-11-06 12:50:17 +0530

    fix routes

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,15 +2,13 @@ var express = require("express");
 const { checkAccessToken } = require("../middlewares/auth.middleware");
 const {
   createAccount,
-  updateAccount,
+
+  withdrawlAccountBalanceController,
 } = require("../controllers/accountController");
 const {
   addAccountValidation,
   updateAccountBalanceValidation,
 } = require("../validators/user/account.validation");
-const {
-  withdrwalAccountBalanceService,
-} = require("../services/account.service");
 var router = express.Router();
 
 router
@@ -18,7 +16,11 @@ router
   .post(checkAccessToken, addAccountValidation, createAccount);
 router
   .route("/transaction/withdrawl-balance/:account_id")
-  .post(checkAccessToken, withdrwalAccountBalanceService, updateAccount);
+  .patch(
+    checkAccessToken,
+    updateAccountBalanceValidation,
+    withdrawlAccountBalanceController
+  );
 // router.route("/account").get();
 
 module.exports = router;
