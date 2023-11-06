891d57a5831920162f8510ba4e06097b03775bbd
krishna jain <krishnajain@gkmit.co>
2023-11-06 17:34:08 +0530

    fix test cases issues

--- helpers/fakeUser.js
+++ helpers/fakeUser.js
@@ -1,4 +1,13 @@
 const { faker } = require("@faker-js/faker");
+
+function generateRandomMobileNumber() {
+  // Generate a random 10-digit number.
+  const mobileNumber = Math.floor(Math.random() * 1000000000) + 1;
+
+  // Return the mobile number as a string.
+  return mobileNumber.toString();
+}
+
 const User = (user = () => {
   return {
     first_name: faker.internet.userName(),
--- test/user.registration.test.js
+++ test/user.registration.test.js
@@ -1,13 +1,12 @@
 const chai = require("chai");
 const expect = chai.expect;
 const chaiHttp = require("chai-http");
-const { User } = require("./../helpers/fakeUser");
 require("dotenv").config();
 chai.use(chaiHttp);
-const userFakeData = require("./../helpers/fakeUser")(User)
+const { User } = require("./../helpers/fakeUser");
 const { BASE_API_URL: api_url } = process.env;
-
-const data = userFakeData();
+const endpoint = "/user/register";
+const data = User();
 console.log(data);
 const invalid_data = {
   first_name: 21321,
@@ -19,7 +18,7 @@ const invalid_data = {
 describe("POST / Describe the user registration", () => {
   it("should send code 201 if user successfully registered ", () => {
     chai
-      .request(url)
+      .request(api_url)
       .post(endpoint)
       .set("Content-Type", "application/json")
       .send(data)
@@ -40,12 +39,11 @@ describe("POST / Describe the user registration", () => {
           "created_at",
           "accessToken"
         );
-        done();
       });
   });
   it("should send code 400 if error for bad request", () => {
     chai
-      .request(url)
+      .request(api_url)
       .post(endpoint)
       .type("form")
       .send(invalid_data)
@@ -55,7 +53,7 @@ describe("POST / Describe the user registration", () => {
   });
   it("should send code 409 if conflict encounter like user already register ", () => {
     chai
-      .request(url)
+      .request(api_url)
       .post(endpoint)
       .type("form")
       .send(data)
@@ -64,13 +62,12 @@ describe("POST / Describe the user registration", () => {
         expect(res.body.code).eql(409);
         expect(res.body).to.have.property("success").equal(false);
         expect(res.body.name).eq("SequelizeUniqueConstraintError");
-        done();
       });
   });
 
   it("should send code 500 if internal server error ", () => {
     chai
-      .request(url)
+      .request(api_url)
       .post(endpoint)
       .type("form")
       .send(data)
