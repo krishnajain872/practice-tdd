25a530d5927947ac288c68373a8820d956fabbd0
krishna jain <krishnajain@gkmit.co>
2023-11-06 18:11:00 +0530

    merge

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,14 @@
-const { errorHelper } = require("../helpers/errorHelp");
-const { userRegistrationService } = require("../services/user.services");
-exports.registerUser = async (req, res) => {
+const express = require("express");
+const User = require("../models/User");
+const { payloadValidate } = require("../helpers/payloadValidationHelper");
+
+async function registerUser(req, res) {
   try {
     // payload
     const payload = req.body;
 
     // validate payload
-    let isNotEmpty = Object.keys(payload).map(
-      (key) => payload[key].length != 0
-    );
-    if (!isNotEmpty) {
+    if (!payloadValidate(payload)) {
       return errorHelper(400, "Bad request", "validation error check payload");
     }
     // service call
@@ -19,35 +18,14 @@ exports.registerUser = async (req, res) => {
     if (response.code === 201 && response.success === true) {
       res.status(201).send(response);
     } else {
-      res.status(response.code).send(response);
+      res.status(201).send("created");
     }
   } catch (err) {
     res.status(500).send(err);
   }
+}
+module.exports = {
+  registerUser,
 };
 
-exports.loginUser = async (req, res) => {
-  try {
-    // payload
-    const payload = req.body;
-
-    // validate payload
-    let isNotEmpty = Object.keys(payload).map(
-      (key) => payload[key].length != 0
-    );
-    if (!isNotEmpty) {
-      return errorHelper(400, "Bad request", "validation error check payload");
-    }
-    // service call
-    const response = await userLoginService(payload);
-
-    console.log("responsev => API CONTROLLER RESPONSE", response);
-    if (response.code === 202 && response.success === true) {
-      res.status(202).send(response);
-    } else {
-      res.status(response.code).send(response);
-    }
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
+exports.loginUser = async (req, res) => {};
