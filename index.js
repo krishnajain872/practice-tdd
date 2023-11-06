52d2e08d1247b10ecdc76a348e6ac5a430d16606
krishna jain <krishnajain@gkmit.co>
2023-11-06 17:10:54 +0530

    fix suggested issues

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = express.Router();
+const api = require("./routes");
 require("dotenv").config();
 const app = express();
 
