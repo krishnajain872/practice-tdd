1eb51286ef6674af620b86d653f5fa91a21845ca
krishna jain <krishnajain@gkmit.co>
2023-11-07 13:39:58 +0530

    fix test cases

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -26,10 +26,21 @@ async function registerUser(req, res) {
     res.status(500).send(err);
   }
 }
-async function loginUser(req, res) {};
+
+async function loginUser(req, res) {
+  try {
+    const payload = req.body
+    if (!payload) {
+      res.status(400).send("invalid data");
+    } else {
+      res.status(202).send("login success")
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+}
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
 };
-
-
