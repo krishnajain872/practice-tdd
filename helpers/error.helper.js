9de1068ba28b0c44f073c451759348fa0c355861
krishna jain <krishnajain@gkmit.co>
2023-11-07 20:05:33 +0530

    fix error helper

--- helpers/error.helper.js
+++ helpers/error.helper.js
@@ -1,11 +1,5 @@
 const errorHelper = (code, name, message, actual) => {
-  if (code == 409 || 422) {
-    type = "service error ";
-  } else if (code == 400) {
-    type = "client error";
-  } else if (code == 500) {
-    type = "Internal server error";
-  }
+ 
   return {
     code: code,
     success: false,
