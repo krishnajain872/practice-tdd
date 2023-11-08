0c44d3ebef4f8f84c7e108dc301a156f9ec9b982
krishna jain <krishnajain@gkmit.co>
2023-11-08 14:46:54 +0530

    fix remove payload validation in controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,13 +5,6 @@ async function registerUser(req, res) {
   try {
     // payload
     const payload = req.body;
-    // validate payload
-    let isNotEmpty = Object.keys(payload).map(
-      (key) => payload[key].length != 0
-    );
-    if (!isNotEmpty) {
-      return errorHelper(400, "Bad request", "validation error check payload");
-    }
     // service call
     const response = await userRegistration(payload);
     if (response.code === 201 && response.success === true) {
