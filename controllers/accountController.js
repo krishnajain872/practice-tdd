332b6ad7ed02e1b0ec0867e8debd1e097896df2f
krishna jain <krishnajain@gkmit.co>
2023-11-08 17:36:36 +0530

    fix service and controller

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,8 +1,8 @@
 const { errorHelper } = require("../helpers/error.helper");
 
-exports.createAccount = async (req, res) => {
-
-   
+async function createAccount(req, res) {
   res.status(202).send("welcome to account");
-
+}
+module.exports = {
+  createAccount,
 };
