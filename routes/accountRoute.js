02479a087ec76f2258f81ec4c35dc6ca0d0dbe51
krishna jain <krishnajain@gkmit.co>
2023-11-09 15:27:04 +0530

    fix routes and env sample

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,11 +1,10 @@
 var express = require("express");
 const { checkAccessToken } = require("../middlewares/auth.middleware");
-const { createAccount } = require("../controllers/accountController");
+const { account } = require("../controllers/accountController");
 const { addAccountValidation } = require("../validators/user/account.validation");
  
 var router = express.Router();
 
-router.route("/create-account").post(checkAccessToken,addAccountValidation,createAccount);
-// router.route("/account").get();
+router.route("/").post(checkAccessToken,addAccountValidation,account);
 
 module.exports = router;
